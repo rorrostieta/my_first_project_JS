22c47f1ee94c9d732153f831b30732d3b62ebfdc
Ruben Orrostieta <ruben.orrostieta@gmail.com>
2022-04-21 21:51:25 -0500

    creacion de index.js con dependencia de /app/missionCommander

---
+++ index.js
@@ -0,0 +1,10 @@
+const MissionCommander = require('./app/missionCommander')
+
+const ruben = new MissionCommander('Ruben')
+console.log(ruben.name)
+
+const jesus = new MissionCommander('Ruben')
+console.log(jesus.name)
+
+const carlo = new MissionCommander('Ruben')
+console.log(carlo.name)
