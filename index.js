faad4d68a4ba9d50d6584b2af221655da2e3d19b
Ruben Orrostieta <ruben.orrostieta@gmail.com>
2022-04-21 21:54:40 -0500

    Correccion de nombres

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const MissionCommander = require('./app/missionCommander')
 const ruben = new MissionCommander('Ruben')
 console.log(ruben.name)
 
-const jesus = new MissionCommander('Ruben')
+const jesus = new MissionCommander('Jesus')
 console.log(jesus.name)
 
-const carlo = new MissionCommander('Ruben')
+const carlo = new MissionCommander('Carlo')
 console.log(carlo.name)
