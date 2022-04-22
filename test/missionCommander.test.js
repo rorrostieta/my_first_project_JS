dde9fd4fc6d2de0464a34855d49a30029ad831db
Ruben Orrostieta <ruben.orrostieta@gmail.com>
2022-04-21 22:09:39 -0500

    Prueba de unidad para objeto missionCommander.js

--- test/missionCommander.test.js
+++ test/missionCommander.test.js
@@ -1,6 +1,8 @@
-describe("Esto es una suite de pruebas", () => {
-  test('Caso de prueba 1', () => {
-    const result = 1 + 2
-    expect(result).toBe(3);
+const MissionCommander = require('./../app/MissionCommander')
+
+describe("Unit Test for Mission Commander Class", () => {
+  test('1. Create a mission commander object', () => {
+    const myMissionCommander = new MissionCommander('Victoria')
+    expect(myMissionCommander.name).toBe('Victoria');
   });
 })
